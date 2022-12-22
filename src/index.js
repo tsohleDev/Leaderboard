a44a15474a28a939f7b38dc624f4ebc1fe47c35d
tsohleDev <mokhemisitsohle@gmail.com>
2022-12-22 13:15:46 +0200

    Integrate API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.scss';
+import ListHTML from "./modules/list.node.js";
+import APi from "./modules/APi.js";
+
+const api = new APi()
+const listNode = new ListHTML(api)
+
+document.querySelector('#submit').addEventListener('click', () => {
+    const name = document.querySelector('#name').value
+    const score = document.querySelector('#score').value
+    console.log(name, score);
+    api.addScore(name, score)
+})
